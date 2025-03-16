edae73a95300fddf44baa19443714e3abc9780ed
SuprimKhatri77 <supremekhatri62@gmail.com>
2025-03-16 21:05:16 +0545

    hooks and useState hooks

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,10 +1,30 @@
+import { useState } from "react";
 import TestFunction from "./Test"
 
 function App() {
+  let [counter, setCounter] = useState(15)
+  
+  const addValue = () => {
+    if(counter < 20){
+
+      counter = counter + 1;
+      setCounter(counter);
+    }
+  }
+  const decreaseValue = () => {
+    if(counter > 0){
+      counter = counter - 1; 
+      setCounter(counter)
+    }
+  }
   return (
     <>
     <TestFunction />
-    <h1>slashy</h1>
+    <h2>Counter value: {counter}</h2>
+    <button onClick={addValue}>Add value</button>
+    <br />
+    <br/>
+    <button onClick={decreaseValue}>Decrease value</button>
     </>
   )
 }
