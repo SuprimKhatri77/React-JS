d8c45f75a2fa3084b8c04983f7aac0df5192212e
SuprimKhatri77 <supremekhatri62@gmail.com>
2025-03-25 14:34:04 +0545

    diff type of form elements and action method

--- freecodecamp-React/13-ReactForms/src/App.jsx
+++ freecodecamp-React/13-ReactForms/src/App.jsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
-
   // const handleSubmit = (event) => {
   //   event.preventDefault()
-    
+
   //   const formData = new FormData(event.currentTarget)
   //   const firstName = formData.get('firstName')
   //   const lastName = formData.get('lastName')
@@ -18,34 +17,36 @@ function App() {
   // function submitViaAPI(data){
   //   console.log(data);
   //   console.log("submitted");
-    
-    
+
   // }
 
   const handleSubmit = (event) => {
-    event.preventDefault()
-    const formElement = event.currentTarget
-    const formData = new FormData(formElement)
-    const email = formData.get('email')
-    const password = formData.get('password')
+    event.preventDefault();
+    const formElement = event.currentTarget;
+    const formData = new FormData(formElement);
+    const email = formData.get("email");
+    const password = formData.get("password");
     // console.log(email);
     // console.log(password);
 
-    formElement.reset()
-    
-    
-  }
+    formElement.reset();
+  };
   const signUp = (formData) => {
-    const email = formData.get('email')    
-    const password = formData.get('password')    
+    const email = formData.get("email");
+    const password = formData.get("password");
+    const description = formData.get("description")
+    const employmentStatus = formData.get("employmentStatus")
     // console.log(email);
     // console.log(password);
     
+    console.log(description);
+    console.log(employmentStatus ? employmentStatus : "employment status not selected");
     
-  }
+    
+  };
   return (
     <>
-       {/* <form method="POST" id="my-form" onClick={handleSubmit}>
+      {/* <form method="POST" id="my-form" onClick={handleSubmit}>
 		<label htmlFor="first-name">First Name: </label>
 		<input type="text" id="first-name" name="firstName" className="input" />
 		<br />
@@ -55,25 +56,54 @@ function App() {
 		<input type="submit" value="Submit" />
 	</form>  */}
 
-
-  <section>
-    <h1>Signup Form</h1>
-    <form action={signUp}>
-      <label htmlFor="email">
-        Email: 
-      <input type="email" name="email" placeholder='email' id="email" />
-      </label>
-      <br /> <br />
-      <label htmlFor="password">
-        Password:
-      <input id="password" type="password" name="password" placeholder='password' />
-      </label> 
-      <br /><br />
-      <button>Submit</button>
-    </form>
-  </section>
+      <section>
+        <h1>Signup Form</h1>
+        <form action={signUp}>
+          <label htmlFor="email">
+            Email:
+            <input type="email" defaultValue="slash@gmail.com" name="email" placeholder="email" id="email" />
+          </label>
+          <br /> <br />
+          <label htmlFor="password">
+            Password:
+            <input
+              id="password"
+              type="password"
+              name="password"
+              placeholder="password"
+              defaultValue="password123"
+            />
+          </label>
+          <br />
+          <br />
+            <label htmlFor="description">
+              Description:
+              <textarea id="description" defaultValue="This is a description" name="description"></textarea>
+            </label>
+            <br />
+            <br />
+          <fieldset>
+            <legend>Employment Status</legend>
+            <label>
+              <input type="radio" name="employmentStatus" value="Unemployed" />
+              Unemployed
+            </label>
+            <label>
+              <input type="radio" name="employmentStatus" value="Full time" />
+              Full time
+            </label>
+            <label>
+              <input type="radio" name="employmentStatus" vlaue="Part time" />
+              Part time
+            </label>
+          </fieldset>
+          <br />
+          <br />
+          <button>Submit</button>
+        </form>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
