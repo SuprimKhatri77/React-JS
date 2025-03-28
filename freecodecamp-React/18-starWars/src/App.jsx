a9ee4fabaa132fd0742a4689d97485e8e026820a
SuprimKhatri77 <supremekhatri62@gmail.com>
2025-03-28 11:19:36 +0545

    fetched starWarsData using aysnc await

--- freecodecamp-React/18-starWars/src/App.jsx
+++ freecodecamp-React/18-starWars/src/App.jsx
@@ -4,31 +4,45 @@ import './App.css'
 function App() {
   const [starWarsData, setStarWarsData] = useState([])
   const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(true)
 
 
-  useEffect(()=>{
-    fetch('https://swapi.dev/api/people')
-        .then(response => response.json())
-        .then(data => setStarWarsData(data["results"]))
-        .catch(error => console.log("Error fetching data: ", error)
-        )
-    
-  },[count])
+  useEffect(() => {
+    async function fetchData () {
+      try{
+        const response = await fetch('https://swapi.dev/api/people')
+        const data = await response.json()
+        setStarWarsData(data["results"])
+      } catch(error) {
+        console.log('error fetching data: ',error)
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchData()
+  },[])
 
   return (
     <>
     <p>The count is  {count} </p>
     <button onClick={ ()=> setCount(prevCount => prevCount + 1)}>Add</button>
-      <div style={{
-        fontSize: "30px",
-        color: "white",
-      }}> 
+    {
+      loading ? (
+        <p>Fetching data ....</p>
+      ) : (
+
+        <div style={{
+          fontSize: "14px",
+          color: "white",
+        }}> 
       {
         starWarsData.map((data,index)=>(
           <p key={index} > {data.name} </p>
         ))
       }
        </div>
+    )
+        }
     </>
   )
 }
