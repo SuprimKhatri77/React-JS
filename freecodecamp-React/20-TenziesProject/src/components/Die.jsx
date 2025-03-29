e90d7a033b228319aeaea05d27fa1335a1022154
SuprimKhatri77 <supremekhatri62@gmail.com>
2025-03-29 14:25:06 +0545

    tenzies project completed

--- freecodecamp-React/20-TenziesProject/src/components/Die.jsx
+++ freecodecamp-React/20-TenziesProject/src/components/Die.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
-function Die({value}) {
+function Die({dice,isHeld,hold,id}) {
   return (
    
-        <button className="shadow-[0px_2px_2px_rgba(0,0,0,0.15)] cursor-pointer rounded-md bg-white h-[50px] w-[50px] flex justify-center items-center font-medium font-['Inter',Arial] text-2xl">
-            {value}
+        <button className={`shadow-[0px_2px_2px_rgba(0,0,0,0.15)] cursor-pointer rounded-md  h-[50px] w-[50px] flex justify-center items-center font-medium font-['Inter',Arial] text-2xl
+        ${isHeld ? "bg-green-400" : "bg-white"}
+        `}
+        onClick={()=> hold(id)}
+        >
+            {dice}
         </button>
   )
 }
--- freecodecamp-React/20-TenziesProject/src/components/Main.jsx
+++ freecodecamp-React/20-TenziesProject/src/components/Main.jsx
@@ -1,9 +1,16 @@
 import React, { useState } from 'react'
 import Die from './Die'
+import {nanoid} from 'nanoid'
 
 function Main() {
 
-    const [value, setValue] = useState(generateAllNewDice())
+    const [dice, setDice] = useState( () =>  generateAllNewDice())
+
+    // const [gameWon , setGameWon] = useState(false)
+
+   const gameWon = dice.length > 0 &&  
+                    dice.every(die => die.isHeld) && 
+                    dice.every(die => die.dice === dice[0].dice)
 
     function generateAllNewDice(){
         // const newDice = []
@@ -16,24 +23,57 @@ function Main() {
         // console.log(newNumbersArr);
         // return newDice
 
-        return new Array(10).fill(0).map(() => Math.floor(Math.random() * 6 + 1))
-        
-        
+        return new Array(10).fill(0).map(() => (
+            {
+                dice: Math.floor(Math.random() * 6 + 1),
+                isHeld: false,
+                id: nanoid()
+        }))   
     }
-    // console.log(generateAllNewDice())
-    // console.log(value);
 
-    const diceNumbers = value.map((item,index) => (
-        <Die key={index} value={item} />
+    // console.log(generateAllNewDice());
+    
+    function rollDice(){
+        setDice(oldDice => 
+            oldDice.map(die => 
+                die.isHeld ? die : {...die, dice : Math.floor(Math.random() * 6 + 1)}
+            )
+        )
+    }
+
+    const diceElements = dice.map((item,index) => (
+        <Die key={item.id} id={item.id} dice={item.dice} isHeld={item.isHeld} hold={hold}/>
     ))
     
+
+    function hold(id){
+        // console.log(id);
+        setDice(oldDice => 
+            oldDice.map(die => 
+                die.id === id ? {...die, isHeld: !die.isHeld} : die
+            )
+        )
+    }
+
+    function newGame () {
+        setDice(generateAllNewDice())
+        // console.log((dice));
+        
+    }
   return (
     <div className='min-h-screen w-screen bg-blue-950 text-blac2 py-5 flex'>
-        <div className='bg-gray-200 mx-5 flex-grow rounded-xl'>
-        <div className='grid grid-cols-5 place-content-center gap-x-4 gap-y-5  h-full max-w-[300px] mx-auto'>
+        <div className='bg-gray-200 mx-5 flex flex-col justify-center items-center gap-10 flex-grow rounded-xl py-5'>
+            <div className='flex flex-col gap-3 items-center'>
+            <h1 className='text-2xl font-bold'>Tenzies</h1>
+            <p className='max-w-3/4 text-center font-medium text-sm'>Roll until all dice are the same. Click each die to freeze it at its current dice between rolls.</p>
+            </div>
+        <div className='grid grid-cols-5 place-content-center gap-x-4 gap-y-5   max-w-[300px] mx-auto'>
         
-            {diceNumbers}
+            {diceElements}
         </div>
+        <button className='w-fit py-2 px-10 text-xl rounded-md text-white cursor-pointer font-bold bg-blue-700 hover:bg-blue-800 transition-all duration-300 ease-in-out' onClick={ gameWon ? newGame : rollDice}>
+            { gameWon ? "New game" : "Roll"}
+        </button>
         </div>
     </div>
   )
